ac1a73de0bb3dd6ad0c63ab38be9732f671d72a6
Mihail Iftodi <mi250228@ncr.com>
2024-08-08 13:43:51 +0300

    Created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
@@ -21,9 +20,6 @@ const devConfig = {
       // shared: ["react", "react-dom"],
       // If you want to make sure all the dependencies are shared and not worry about what modules are shared
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
