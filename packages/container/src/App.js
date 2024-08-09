191db1834e0df41bd13672f1455df73abe35d097
Mihail Iftodi <mi250228@ncr.com>
2024-08-09 11:57:17 +0300

    Added dashboard subapp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import React, { Suspense, lazy, useState } from "react";
+import React, { Suspense, lazy, useEffect, useState } from "react";
+import { Redirect, Route, Router, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
@@ -7,19 +7,29 @@ import {
 
 import Header from "./components/Header";
 import Progress from "./components/Progress";
+import { createBrowserHistory } from "history";
 
 const MarketingApp = lazy(() => import("./components/MarketingApp"));
 const AuthApp = lazy(() => import("./components/AuthApp"));
+const DashboardApp = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    }
+  }, [isSignedIn]);
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header
@@ -31,11 +41,15 @@ export default () => {
               <Route path="/auth">
                 <AuthApp onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardApp />
+              </Route>
               <Route path="/" component={MarketingApp} />
             </Switch>
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,17 @@
+import Dashboard from "./components/Dashboard.vue";
+import { createApp } from "vue";
+
+const mount = (el) => {
+  const app = createApp(Dashboard);
+  app.mount(el);
+};
+
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.getElementById("_dashboard-dev-root");
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+export { mount };
